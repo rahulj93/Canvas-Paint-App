be0afa0f78c91bef3411b7f62f5723bb7fc48b8e
Rahul Jain <rahulj93@gmail.com>
2020-11-11 01:03:40 -0500

    functioning canvas with user input

--- paint.js
+++ paint.js
@@ -1,14 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
   const graffitiWall = document.getElementById("graffiti_wall"); 
-  // graffitiWall.width = 750;
-  // graffitiWall.height = 750; 
 
   const ctx = graffitiWall.getContext("2d"); 
-  ctx.fillStyle = "gray";
-  // ctx.fillRect(0,0,750,750);
-  // ctx.fillRect(0,0,1000,1000);
-  ctx.fillRect(0,0,10000, 10000);
-  ctx.fillRect();
+
+  let painting = document.getElementById("content"); 
+  let paintStyle = getComputedStyle(painting);
+  graffitiWall.width = parseInt(paintStyle.getPropertyValue("width")); 
+  graffitiWall.height = parseInt(paintStyle.getPropertyValue("height")); 
+
+  let mouse = {x: 0, y: 0}; 
+
+  graffitiWall.addEventListener('mousemove', function(e) {
+    mouse.x = e.pageX - this.offsetLeft; 
+    mouse.y = e.pageY - this.offsetTop; 
+    // - graffitiWall.height; 
+    // mouse.y = graffitiWall.height - this.offsetTop; 
+  }, false); 
+
+  ctx.lineWidth = 3; 
+  ctx.lineJoin = 'round'; 
+  ctx.lineCap = 'round'; 
+  ctx.strokeStyle = 'black'; 
+
+  graffitiWall.addEventListener('mousedown', function(e) {
+    ctx.beginPath(); 
+    ctx.moveTo(mouse.x, mouse.y); 
+    graffitiWall.addEventListener('mousemove', onPaint, false); 
+  }, false); 
+
+  graffitiWall.addEventListener('mouseup', function() {
+    graffitiWall.removeEventListener('mousemove', onPaint, false)
+  }, false); 
+
+  let onPaint = function() {
+    ctx.lineTo(mouse.x, mouse.y);
+    ctx.stroke(); 
+  }
 
 
 });
